368b7b77f1a3a4f27d784d851dea764e688bdf10
25hokins25 <25hokins25@gmail.com>
2023-07-21 14:29:14 -0700

    Add header.util.js

---
+++ cypress/specs/regression/header.js
@@ -0,0 +1,8 @@
+import Header from '../../utils/elements/header.util'
+
+describe('Elements', () => {
+    before(() => cy.visit('/'));
+    it('Header is displayed', () => {
+        
+      })
+});
--- cypress/utils/elements/header.util.js
+++ cypress/utils/elements/header.util.js
@@ -1,11 +1,34 @@
 // selectors
 const selectors = {
-    
+    mainPanel: '#mainPanel',
+    topPanel: '#topPanel',
+    headerPanel: '#headerPanel',
+    bodyPanel: '#bodyPanel',
+    logo: '.admin',
+    bankLogo: '.logo',
+    slogan: 'p[class="caption"]',
+    link1: 'li[class="Solutions"]',
+    link2: '.leftmenu > li > a[href*="about"]', 
+    link3: '.leftmenu > li > a[href*="services"]',
+    link4: '.leftmenu > li > a[href*="products"]',
+    link5: '.leftmenu > li > a[href*="contacts"]',
+    link6: '.leftmenu > li > a[href*="admin"]',
+    btnHome: '.button > li[class="home"]',
+    btnAbout: '.button > li[class="about"]',
+    btnContact: '.button > li[class="contact"]',
 }
 
 //expected result
 const expected = {
-   
+   logo: '/parabank/images/clear.gif',
+   bankLogo: '/parabank/images/logo.gif',
+   link1Text: 'Solution',
+   link2Text: 'About Us',
+   link3Text: 'Services',
+   link4Text: 'Products',
+   link5Text: 'Locations',
+   link6Text: 'Admin Page',
+   slogan: 'Experience the difference',
 }
 
 export default {
