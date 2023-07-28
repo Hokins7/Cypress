8a7543d011e8cec7d5304c1834d98d02d6ee9183
25hokins25 <25hokins25@gmail.com>
2023-07-28 09:51:20 -0700

    login.util file edited

--- cypress/utils/pages/login.util.js
+++ cypress/utils/pages/login.util.js
@@ -1,35 +1,47 @@
 //selectors
 const selectors = {
-  
+   leftPanel: '#leftPanel',
+   heading: '#leftPanel > h2',
+   loginPanel: '#loginPanel',
+   loginSectionHeading: 'form[name="login"] > p:nth-child(1)',
+   passSectionHeading: 'form[name="login"] > p:nth-child(2)',
+   loginField: 'imput[type="text"]',
+   passField: 'input[type="password"]',
+   logginBtn: 'input[type="submit"]',
+   forgotLink: 'a[href="lookup.htm"]',
+   registerLink: 'a[href="register.htm"]',
 }
 
 //expectede results
 const expected = {
- 
+   heading: 'Customer Login',
+   loginSectionHeading: 'Username',
+   passSectionHeading: 'Password',
+   logginBtnText: 'Log In',
+   forgotLinkText: 'Forgot login info?',
+   registerLinkText: 'Register',
+   login: 'Tester',
+   password: 'TesterPass'
 }
 
  // Fuctions
- const openPage = () => {
-    cy.visit('/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/');
- };
- const checkPage = () => {
-    cy.get(selectors.loginPage).should('be.visible');
+ const checkLoginSection = () => {
+    cy.get(selectors.loginPanel).should('be.visible');
  }
- const enterEmail  = (value) => {
-    cy.get(selectors.emailInput).clear()
-    cy.get(selectors.emailInput).type(value)
+ const enterUsername  = (value) => {
+    cy.get(selectors.loginField).clear()
+    cy.get(selectors.loginField).type(value)
  };
 
  const enterPassword  = (value) => {
-    cy.get(selectors.passInput).clear()
-    cy.get(selectors.passInput).type(value)
+    cy.get(selectors.passField).clear()
+    cy.get(selectors.passField).type(value)
  };
 
 export default {
     ...selectors,
     expected,
-    openPage,
-    enterEmail,
+    enterUsername,
     enterPassword,
-    checkPage
+    checkLoginSection
 }
