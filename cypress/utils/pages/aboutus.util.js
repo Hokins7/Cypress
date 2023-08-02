821b78b895213d58762926d464ebf2ee67e2a0b2
25hokins25 <25hokins25@gmail.com>
2023-08-02 16:52:01 -0700

    Delete some code

--- cypress/utils/pages/aboutus.util.js
+++ cypress/utils/pages/aboutus.util.js
@@ -11,7 +11,7 @@ const expected = {
     firstParagraphTxt: 'ParaBank is a demo site used for demonstration of Parasoft software solutions. All materials herein are used solely for simulating a realistic online banking website.',
     secParagraphTxt: 'In other words: ParaBank is not a real bank!',
     thrdParagraphTxt: 'For more information about Parasoft solutions please visit us at: www.parasoft.com or call 888-305-0041',
-    aboutUsLink: 'parabank/about.htm',
+    
     parasoftLink: 'https://www.parasoft.com/'
 }
 
