8d26b2df507a4f321bafcabc7f7e6f7a35cd19ea
25hokins25 <25hokins25@gmail.com>
2023-08-03 11:55:02 -0700

    Finish with contact.util

--- cypress/utils/pages/contact.util.js
+++ cypress/utils/pages/contact.util.js
@@ -7,11 +7,21 @@ const selectors = {
     inputEmail: '#email',
     inputPhone: '#phone',
     inputMessage: '#message',
-    
+    nameLabel: 'td[align="right"] > b:nth-child(1)',
+    emailLabel: 'td[align="right"] > b:nth-child(2)',
+    phoneLabel: 'td[align="right"] > b:nth-child(3)',
+    messageLabel: 'td[align="right"] > b:nth-child(4)',
+    sendBtn: 'input[type="submit"]:nth-child(1)'
 }
 
 const expected = {
-
+    tittleTxt: 'Customer Care',
+    subTittleTxt: 'Email support is available by filling out the following form.',
+    nameTxt: 'Name:',
+    emailTxt: 'Email:',
+    phoneTxt: 'Phone:',
+    messageTxt: 'Message:',
+    sendBtnTxt: 'Send to Customer Care'
 }
 
 export default {
