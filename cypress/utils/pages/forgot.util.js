175fdb4d445823b464bad4f3ac8202205f74dbb0
25hokins25 <25hokins25@gmail.com>
2023-08-17 11:43:56 -0700

    Finish create selectors

--- cypress/utils/pages/forgot.util.js
+++ cypress/utils/pages/forgot.util.js
@@ -4,6 +4,18 @@ const selectors = {
     forgotForm: '#lookupForm',
     firstNameTxt: '#lookupForm > table > tbody > tr:nth-child(1) > td:nth-child(1) > b',
     firstNameFrm: '#firstName',
-    lastNameTxt: '',
-    lastNameFrm: ''
+    lastNameTxt: '#lookupForm > table > tbody > tr:nth-child(2) > td:nth-child(1) > b',
+    lastNameFrm: '#lastName',
+    addressStrTxt: '#lookupForm > table > tbody > tr:nth-child(3) > td:nth-child(1) > b',
+    addressStrFrm: '#address.street',
+    addressCityTxt: '#lookupForm > table > tbody > tr:nth-child(4) > td:nth-child(1) > b',
+    addressCityFrm: '#address.city',
+    addressStateTxt: '#lookupForm > table > tbody > tr:nth-child(5) > td:nth-child(1) > b',
+    addressStateFrm: '#address.state',
+    zipCodeTxt: '#lookupForm > table > tbody > tr:nth-child(6) > td:nth-child(1) > b',
+    zipCodeFrm:'#address.zipCode',
+    ssnTxt: '#lookupForm > table > tbody > tr:nth-child(7) > td:nth-child(1) > b',
+    ssnFrm: '#ssn',
+    submitBtn: 'input[value="Find My Login Info"]'
+
 }
