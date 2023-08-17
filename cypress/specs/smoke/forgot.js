c958012e30d1303ea8fd8618255e14aa4588c40a
25hokins25 <25hokins25@gmail.com>
2023-08-17 12:00:30 -0700

    Testing elements

--- cypress/specs/smoke/forgot.js
+++ cypress/specs/smoke/forgot.js
@@ -1,7 +1,22 @@
 import Forgot from '../../utils/pages/forgot.util'
+import MainPage from '../../utils/pages/main_page.util'
 
 describe ('Elements', () => {
 
-    before(() => cy.visit('/'));
+    before(() => cy.visit(Forgot.expected.link));
+
+    it('Main panel is displayed', () => {
+        cy.get(MainPage.rightPanel).should('be.visible');
+    })
+    it('Tittle is visible and has correct text', () => {
+        cy.get(Forgot.tittle())
+            .should('have.text', Forgot.expected.tittle)
+            .and('be.visible');
+    })
+    it('Subtittle is visible and has correct text', () => {
+        cy.get(Forgot.subTittle())
+            .should('have.text', Forgot.expected.subTittle)
+            .and('be.visible');
+    })
 
 })
