7b813c577dd96b8425229261d75e063b5bfb0a21
25hokins25 <25hokins25@gmail.com>
2023-08-18 11:49:15 -0700

    Continue working with forgot.js

--- cypress/specs/smoke/forgot.js
+++ cypress/specs/smoke/forgot.js
@@ -23,5 +23,16 @@ describe ('Elements', () => {
             .should('have.text', Forgot.expected.firstNameTxt)
             .and('be.visible');
     })
+    it('Lastname text is visible and has a correct text', () => {
+        cy.get(Forgot.lastNameTxt)
+            .should('have.text', Forgot.expected.lastNameTxt)
+            .and('be.visible');
+    })
+    it('Address text is visible and has a correct text', () => {
+        cy.get(Forgot.addressStrTxt)
+            .should('have.text', Forgot.expected.addressStrTxt)
+            .and('be.visible');
+    })
+
 
 })
