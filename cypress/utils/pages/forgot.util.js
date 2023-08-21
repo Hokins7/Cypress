4417227fe1712795c21f64ef5f68661363c04aec
25hokins25 <25hokins25@gmail.com>
2023-08-21 13:18:24 -0700

    Selectors changed

--- cypress/utils/pages/forgot.util.js
+++ cypress/utils/pages/forgot.util.js
@@ -7,16 +7,17 @@ const selectors = {
     lastNameTxt: '#lookupForm > table > tbody > tr:nth-child(2) > td:nth-child(1) > b',
     lastNameFrm: '#lastName',
     addressStrTxt: '#lookupForm > table > tbody > tr:nth-child(3) > td:nth-child(1) > b',
-    addressStrFrm: '[id="address.street"]',
+    addressStrFrm: "#address\\.street",
     addressCityTxt: '#lookupForm > table > tbody > tr:nth-child(4) > td:nth-child(1) > b',
-    addressCityFrm: '[id="address.city"]',
+    addressCityFrm: '#address\\.city',
     addressStateTxt: '#lookupForm > table > tbody > tr:nth-child(5) > td:nth-child(1) > b',
-    addressStateFrm: '[id="address.state"]',
+    addressStateFrm: '#address\\.state',
     zipCodeTxt: '#lookupForm > table > tbody > tr:nth-child(6) > td:nth-child(1) > b',
-    zipCodeFrm:'[id="address.zipCode"]',
+    zipCodeFrm:'#address\\.zipCode',
     ssnTxt: '#lookupForm > table > tbody > tr:nth-child(7) > td:nth-child(1) > b',
     ssnFrm: '#ssn',
-    submitBtn: 'input[value="Find My Login Info"]'
+    submitBtn: 'input[value="Find My Login Info"]',
+
 }
 
 const expected = {
