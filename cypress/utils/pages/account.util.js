765a4440786e1f3a8dcefdddaaf0a3a267e41a0d
25hokins25 <25hokins25@gmail.com>
2023-08-29 12:58:55 -0700

    Continue create selectors

--- cypress/utils/pages/account.util.js
+++ cypress/utils/pages/account.util.js
@@ -1,8 +1,17 @@
 const selectors = {
-    smallext: '.smallText',
+    smallText: '.smallText',
+    openNewAccLink: 'a[href="/parabank/openaccount.htm"]',
+    accOverview: 'a[href="/parabank/overview.htm"]',
+
+
+
 }
 
 const expected = {
+    smallText: `Welcome ${Cypress.env().FIRST_NAME} ${Cypress.env().LAST_NAME}`,
+    openNewAccLink: 'Open New Account',
+    accOverview: 'Accounts Overview',
+    
 
 }
 
