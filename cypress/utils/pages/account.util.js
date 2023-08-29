efaefdfa406a473d31dc56f041446d6d70c6c918
25hokins25 <25hokins25@gmail.com>
2023-08-29 13:10:20 -0700

    Add some selectors from other file

--- cypress/utils/pages/account.util.js
+++ cypress/utils/pages/account.util.js
@@ -1,21 +1,34 @@
 const selectors = {
     smallText: '.smallText',
     openNewAccLink: 'a[href="/parabank/openaccount.htm"]',
-    accOverview: 'a[href="/parabank/overview.htm"]',
-
-
-
+    accOverviewLink: 'a[href="/parabank/overview.htm"]',
+    transfFundsLink: 'a[href="/parabank/transfer.htm"]',
+    billPayLink: 'a[href="/parabank/billpay.htm"]',
+    findTransactionLink: 'a[href="/parabank/findtrans.htm"]',
+    updateContInfoLink: 'a[href="/parabank/updateprofile.htm"]',
+    requestLoanLink: 'a[href="/parabank/requestloan.htm"]',
+    logOutBtn: 'a[href="/parabank/logout.htm"]'
 }
 
 const expected = {
     smallText: `Welcome ${Cypress.env().FIRST_NAME} ${Cypress.env().LAST_NAME}`,
-    openNewAccLink: 'Open New Account',
-    accOverview: 'Accounts Overview',
-    
+    openNewAccTxt: 'Open New Account',
+    accOverviewTxt: 'Accounts Overview',
+    transfFundsTxt: 'Transfer Funds',
+    billPayTxt: 'Bill Pay',
+    findTransactionTxt: 'Find Transactions',
+    updateContInfoTxt: 'Update Contact Info',
+    requestLoanTxt: 'Request Loan',
+    logOutBtnTxt: 'Log Out'
 
 }
 
+const LogOut = () => {
+    cy.get(selectors.logOutBtn).click();
+  }
+
 export default {
     ...selectors,
-    expected
+    expected,
+    LogOut
 }
