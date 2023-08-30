dd7e72527fc8f24e176b1ff977af7c065417ff79
25hokins25 <25hokins25@gmail.com>
2023-08-30 11:57:35 -0700

    Command add ClickIfExist

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import Account from '../utils/pages/account.util'
+
+Cypress.Commands.add('clickIfExists', (selector) => {
+    cy.get(selector).should('exist').click();
+  });
