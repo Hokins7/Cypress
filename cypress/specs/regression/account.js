51b002cfb27f1e639d9cb5e29d1093015d43bdda
25hokins25 <25hokins25@gmail.com>
2023-08-31 13:31:33 -0700

    Add welcome message test

--- cypress/specs/regression/account.js
+++ cypress/specs/regression/account.js
@@ -1,7 +1,11 @@
 import Account from '../../utils/pages/account.util'
 
 describe('Elements', () => {
-
+    it('Welcome message visible and correct' , () => {
+        cy.get(Account.smallText)
+            .should('be.visible')
+            .and('have.text', Account.expected.smallText);
+    })
 })
 
 describe('Functionality', () => {
