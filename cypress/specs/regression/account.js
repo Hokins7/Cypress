e4b8ac783d5d4c1396381b892adbb0fd63c783cd
25hokins25 <25hokins25@gmail.com>
2023-08-31 13:34:37 -0700

    Continue write tests

--- cypress/specs/regression/account.js
+++ cypress/specs/regression/account.js
@@ -1,10 +1,25 @@
 import Account from '../../utils/pages/account.util'
 
 describe('Elements', () => {
-    it('Welcome message visible and correct' , () => {
+    it('Welcome message visible and correct', () => {
         cy.get(Account.smallText)
             .should('be.visible')
             .and('have.text', Account.expected.smallText);
+    })
+    it('First link visible and have a correct text', () => {
+        cy.get(Account.openNewAccLink)
+            .should('be.visible')
+            .and('have.text', Account.expected.openNewAccTxt);
+    })
+    it('Second link visible and have a correct text', () => {
+        cy.get(Account.accOverviewLink)
+            .should('be.visible')
+            .and('have.text', Account.expected.accOverviewTxt);
+    })
+    it('Third link visible and have a correct text', () => {
+        cy.get(Account.transfFundsLink)
+            .should('be.visible')
+            .and('have.text', Account.expected.transfFundsTxt);
     })
 })
 
