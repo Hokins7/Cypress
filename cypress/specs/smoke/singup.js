500efcf30fe88779a03b1891b308a0de315e4451
25hokins25 <25hokins25@gmail.com>
2023-08-31 13:29:13 -0700

    Add logOut comand

--- cypress/specs/smoke/singup.js
+++ cypress/specs/smoke/singup.js
@@ -1,4 +1,5 @@
-import SingUp from '../../utils/pages/singup.util'
+import SingUp from '../../utils/pages/singup.util';
+import Account from '../../utils/pages/account.util'
 
 describe ('Create account', () => {
     before(() => cy.visit(SingUp.expected.singupPageLink));
@@ -6,5 +7,6 @@ describe ('Create account', () => {
     it('Creating account', () => {
         SingUp.fuctions.createAcoount();
         cy.get(SingUp.welcomeMsg).should('have.text', SingUp.expected.welcomeMsg);
+        cy.get(Account.logOutBtn).click();
     })
 })
